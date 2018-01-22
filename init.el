93fa35d52398bd109d7e1f81a92178c5147654e5
drkmrin78 <petersendrake@gmail.com>
2018-01-22 16:07:02 -0500

    font, new machine

--- init.el
+++ init.el
@@ -147,5 +147,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight normal :height 127 :width normal))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal))))
  '(region ((t (:background "light slate gray")))))
