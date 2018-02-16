c61fa438eee3bda10ee02458797ab482e69fe93f
drkmrin78 <petersendrake@gmail.com>
2018-02-15 21:14:25 -0500

    something

--- init.el
+++ init.el
@@ -117,6 +117,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(delete-selection-mode nil)
  '(inhibit-startup-screen t)
  '(initial-scratch-message
