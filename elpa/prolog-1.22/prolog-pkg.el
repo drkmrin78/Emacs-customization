0ddc3835fc186b1b07554eab4b9da27a317c7322
drkmrin78 <petersendrake@gmail.com>
2018-03-21 20:06:23 -0400

    hiding elpa from git

--- elpa/prolog-1.22/prolog-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "prolog" "1.22" "major mode for editing and running Prolog (and Mercury) code" 'nil)
--- elpa/undo-tree-20170706.246/undo-tree-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "undo-tree" "20170706.246" "Treat undo history as a tree" (quote nil))
